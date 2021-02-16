45acd39dc4ee01bc974be8a49273c13d23c08bc5
scott2b <scottbradley.mail@gmail.com>
2021-02-16 16:14:35 -0600

    adds setup.py

---
+++ scai/code/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(name='scai-classification',
+      version='1.0',
+      description='Classification models.',
+      author='scott2b',
+      author_email='scott@example.com',
+      url='https://github.com/scott2b/amazon-sagemaker-script-mode',
+      packages=find_packages(exclude=('tests', 'docs')))
